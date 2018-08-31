013e320e1cb06584434c30178e0ec25e2fb81fc7
Peter <pnolan1986@gmail.com>
2018-08-31 14:27:14 -0400

    work on extracting lines

--- eulerian_ridges.py
+++ eulerian_ridges.py
@@ -11,8 +11,8 @@ grid_spacing = 12*1000 #km 2 m
 tdim = 25
 xdim = 102
 ydim = 82
-
-root = Dataset('subset_wrfout_d01_2011-07-01_00_00_00','r')
+#root = Dataset('subset_wrfout_d01_2011-07-01_00_00_00','r')
+root = Dataset('wrf_2011_07_01','r')
 cen_lat = getattr(root,'CEN_LAT')
 cen_lon = getattr(root,'CEN_LON')
 true_lat1 = getattr(root,'TRUELAT1')
@@ -28,7 +28,8 @@ root.close()
 
 checklon, checklat = mf.lonlat2km(ref_lon,ref_lat,lon,lat,true_lat1,true_lat2) 
 
-root = Dataset('subset_wrfout_d01_2011-07-02_00_00_00','r')
+#root = Dataset('subset_wrfout_d01_2011-07-02_00_00_00','r')
+root = Dataset('wrf_2011_07_02','r')
 vars = root.variables
 u = np.concatenate((u,vars['U'][:,height_level,:,:]))
 v = np.concatenate((v,vars['V'][:,height_level,:,:]))
@@ -38,7 +39,7 @@ v = mf.unstagger(v[:25,:,:],1)
 '''
 u=u[-1,:,:]
 v=v[-1,:,:]
-'''
+#'''
 u=u[0,:,:]
 v=v[0,:,:]
 #lon = lon[-1,:,:]
@@ -121,6 +122,10 @@ for i in range(ydim):
             rdirdiv[i,j] = np.ma.masked
             rconcav[i,j] = np.ma.masked
 
+adirdiv = np.ma.masked_where(aconcav<0,adirdiv)
+adirdiv = np.ma.masked_where(-s1<=np.percentile(-s1,85),adirdiv)
+rdirdiv = np.ma.masked_where(rconcav>0,rdirdiv)
+rdirdiv = np.ma.masked_where(s2<=np.percentile(s2,85),rdirdiv)
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
@@ -142,20 +147,18 @@ m = Basemap(llcrnrlon=lon_min,
             urcrnrlon=lon_max,
             urcrnrlat=lat_max,
             projection='merc',
-            resolution = 'i',
-            area_thresh=1000.,
+            resolution = 'l',
+            area_thresh=5000.,
             )
-#'''
 parallels = np.arange(round(lat_min,0),lat_max+2,2)
 meridians = np.arange(round(lon_max,0),lon_min-2,-2)
 
+'''
 fig = plt.figure(1)
 plt.subplot(121)
 #lon,lat = np.meshgrid(lon,lat)
 cs = m.contourf(lon,lat,s1,levels=np.linspace(np.min(s1,axis=None),np.max(s1,axis=None),301),latlon=True)
 plt.colorbar()
-adirdiv = np.ma.masked_where(aconcav<0,adirdiv)
-adirdiv = np.ma.masked_where(-s1<=np.percentile(-s1,85),adirdiv)
 ridge = m.contour(lon,lat,adirdiv,levels=0,latlon=True,colors='cyan')
 m.drawcoastlines()
 m.drawstates()
@@ -168,8 +171,6 @@ plt.subplot(122)
 #lon,lat = np.meshgrid(lon,lat)
 cs = m.contourf(lon,lat,s2,levels=np.linspace(np.min(s2,axis=None),np.max(s2,axis=None),301),latlon=True)
 plt.colorbar()
-rdirdiv = np.ma.masked_where(rconcav>0,rdirdiv)
-rdirdiv = np.ma.masked_where(s2<=np.percentile(s2,85),rdirdiv)
 ridge = m.contour(lon,lat,rdirdiv,levels=0,latlon=True,colors='magenta')
 m.drawcoastlines()
 m.drawstates()
@@ -179,7 +180,7 @@ m.drawparallels(parallels,labels=[1,0,0,0],fontsize=10)
 m.drawmeridians(meridians,labels=[0,0,0,1],fontsize=10)
 
 fig = plt.figure(2)
-m.quiver(lon[::5,::5],lat[::5,::5],u[::5,::5],v[::5,::5],latlon=True)
+#m.quiver(lon[::5,::5],lat[::5,::5],u[::5,::5],v[::5,::5],latlon=True)
 ridge = m.contour(lon,lat,adirdiv,levels=0,latlon=True,colors='blue')
 ridge = m.contour(lon,lat,rdirdiv,levels=0,latlon=True,colors='red')
 #ridge = m.contour(lon,lat,det,levels=0,latlon=True)
@@ -187,7 +188,7 @@ ax = plt.gca()
 def format_coord(x, y):
     return 'x={0[0]:.4f}, y={0[1]:.4f}'.format(m(x, y, inverse = True))
 ax.format_coord = format_coord
-r=20#km
+r=15#km
 theta = np.arange(0,2*np.pi,0.1)
 x = r*np.cos(theta)
 y = r*np.sin(theta)
@@ -209,8 +210,8 @@ parallels = np.arange(round(lat_min,0),lat_max+2,2)
 meridians = np.arange(round(lon_max,0),lon_min-2,-2)
 m.drawparallels(parallels,labels=[1,0,0,0],fontsize=10)
 m.drawmeridians(meridians,labels=[0,0,0,1],fontsize=10)
+#'''
 '''
-46
 v = ridge.collections[0].get_paths()[40].vertices
 x,y = m(v[:,0],v[:,1],inverse=True)
 v = np.stack((x,y),axis=1)
@@ -221,21 +222,87 @@ with open("ridge.txt", "w", newline='') as f:
     f.close()
 
 
-''
+#'''
+'''
+aridge = m.contour(lon,lat,adirdiv,levels=0,latlon=True,colors='blue')
+rridge = m.contour(lon,lat,rdirdiv,levels=0,latlon=True,colors='red')
+m.drawcoastlines()
+plt.savefig('{:03d}_all.png'.format(000))
 plt.close('all')
-pp = ridge.collections[0].get_paths()
+pp = aridge.collections[0].get_paths()
 for p in range(len(pp)):
     v = pp[p].vertices
     x = v[:,0]
     y = v[:,1]
-    if x.size > 10:
-        m.plot(x,y)#, latlon=True)
+    if x.size > 6:
+        m.plot(x,y,'b-')#, latlon=True)
         m.drawcoastlines()
         plt.title('{:03d}'.format(p))
-        plt.savefig('{:03d}.png'.format(p))
+        plt.savefig('{:03d}_attracting.png'.format(p))
         plt.close('all')
-        
-plt.show()        
+pp = rridge.collections[0].get_paths()
+for p in range(len(pp)):
+    v = pp[p].vertices
+    x = v[:,0]
+    y = v[:,1]
+    if x.size > 6:
+        m.plot(x,y,'r-')#, latlon=True)
+        m.drawcoastlines()
+        plt.title('{:03d}'.format(p))
+        plt.savefig('{:03d}_repelling.png'.format(p))
+        plt.close('all')
+#'''
+aridge = m.contour(lon,lat,adirdiv,levels=0,latlon=True,colors='blue')
+rridge = m.contour(lon,lat,rdirdiv,levels=0,latlon=True,colors='red')
+plt.close('all')
+pp = aridge.collections[0].get_paths()
+for p in [13,17,40,46,47,55]:
+    v = pp[p].vertices
+    x = v[:,0]
+    y = v[:,1]
+    m.plot(x,y,'b-')#, latlon=True)
+pp = rridge.collections[0].get_paths()
+for p in [8,18,19,53,56,60]:
+    v = pp[p].vertices
+    x = v[:,0]
+    y = v[:,1]
+    m.plot(x,y,'r-')#, latlon=True)
+    
+   
+
+m.drawcoastlines()
+plt.savefig('__First.png')
+
+ax = plt.gca()
+def format_coord(x, y):
+    return 'x={0[0]:.4f}, y={0[1]:.4f}'.format(m(x, y, inverse = True))
+ax.format_coord = format_coord
+r=15#km
+theta = np.arange(0,2*np.pi,0.1)
+xx = r*np.cos(theta)
+yy = r*np.sin(theta)
+#x = x+967075/500
+#y = y-694557/1000
+latlon=[(-80.3136,33.5022),(-79.6674,34.3957)]
+x=[]
+y=[]
+for i, latlon in enumerate(latlon):
+    print(i)
+    print(latlon[0])
+    print(latlon[1])
+    w,z = mf.km2lonlat(latlon[0],latlon[1],xx,yy,true_lat1,true_lat2)
+    x.append(w)
+    y.append(z)
+m.scatter(x,y,latlon=True)
+
+     
+'''
+r=15#km
+theta = np.arange(0,2*np.pi,0.1)
+x = r*np.cos(theta)
+y = r*np.sin(theta)
+x,y = mf.km2lonlat(-80.3136,33.5022,x,y,true_lat1,true_lat2)
+m.scatter(x,y,latlon=True)
 #'''
 
 
