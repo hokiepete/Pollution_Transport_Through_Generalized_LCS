65ed5e57a95b02d5081c5301f5da91324ca5561a
Peter <pnolan1986@gmail.com>
2018-12-07 14:56:59 -0500

    update

--- oecs_3d.m
+++ oecs_3d.m
@@ -3,7 +3,7 @@ close all
 clc
 load oecs_data
 
-for i=1:5
+for i=1:10
     dirdiv1 = smooth3(dirdiv1);
     concav1 = smooth3(concav1);
     s1 = smooth3(s1);
@@ -12,7 +12,7 @@ dirdiv1(s1>0)=NaN;
 dirdiv1(concav1<=0)=NaN;
 %dirdiv1(concav1<=0)=NaN;
 figure
-vol3d_v2('cdata',dirdiv1(:,:,4:end))
+vol3d_v2('cdata',dirdiv1(:,:,1:end))
 colorbar
 figure
 isosurface(x(:,:,4:end),y(:,:,4:end),z(:,:,4:end),dirdiv1(:,:,4:end),0,'verbose')
